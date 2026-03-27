5a278cf01af61efc0a5980882784bb5c1487048c
faraiba <faraibafarnantabinda@gmail.com>
2026-03-27 13:37:03 +0000

    initial

---
+++ QApp/Backend/data_access.py
@@ -0,0 +1,8 @@
+DOCS = {
+ "docker": "Docker packages applications and their dependencies.",
+ "image": "An image is a blueprint used to create containers.",
+ "container": "A container is a running instance of an image.",
+ "port": "Port mapping connects your machine to a port inside the container."
+}
+def get_docs():
+ return DOCS
